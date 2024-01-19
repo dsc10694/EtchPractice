882825746a644c1b9bc3e700ce9a4d2993e31350
dsc10694 <136835709+dsc10694@users.noreply.github.com>
2024-01-19 00:10:58 -0500

    add comments

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-const gridesize = 600
-let squarePerSide = 16
-const sketchArea = document.getElementById('sketch');
-const cellContainer = document.createElement('div')
-const noButton = document.querySelector('.noButton')
-const button = document.querySelector('.reset')
+const gridesize = 600  //value for size of grid
+let squarePerSide = 16     //initial value of each axis of grid
+const sketchArea = document.getElementById('sketch');   
+const cellContainer = document.createElement('div')     //create container to hold cells
+const noButton = document.querySelector('.noButton')        //button for number of squares per side
+const button = document.querySelector('.reset')       //reset button
 
 
 
-sketchArea.style.width = sketchArea.style.height = `${gridesize}px`;
+sketchArea.style.width = sketchArea.style.height = `${gridesize}px`;  //set value of height and with of sketch area
     
  
-function changeBackgroundColor(){
-    this.style.backgroundColor = 'black';
-}
-function createGrid()  { 
-    let totalsqure = squarePerSide*squarePerSide;
-    const widthHeight = `${gridesize/squarePerSide-2}px`
 
-for (i=0; i<totalsqure; i++){
+function createGrid()  {  //function to create grid
+    let totalsqure = squarePerSide*squarePerSide; //value for Length by width 
+    const widthHeight = `${gridesize/squarePerSide-2}px` //create width and height of each square
+
+for (i=0; i<totalsqure; i++){  //for loop  to create each grid
     const    gridCell = document.createElement('div');
-    gridCell.style.backgroundColor = 'white'
+    gridCell.style.backgroundColor = 'white'  //set color of white background for each cell
     
     
-    gridCell.style.width =widthHeight
-    gridCell.style.height = widthHeight
-    gridCell.classList.add('cell')
+    gridCell.style.width =widthHeight  //set width of cell
+    gridCell.style.height = widthHeight //set height of cells
+    gridCell.classList.add('cell') //add class list to gridcell for use in CSS
     
-    sketchArea.appendChild(gridCell)
-    gridCell.addEventListener('mouseover',changeBackgroundColor)   
-    button.addEventListener('click', () => {
+    sketchArea.appendChild(gridCell) //add gridcell to sketch area
+    function changeBackgroundColor(){     //color  change which will be put in mouse over event listener
+        gridCell.style.backgroundColor = 'black';
+    }
+    gridCell.addEventListener('mouseover',changeBackgroundColor)    //change background color on hover
+    button.addEventListener('click', () => { // when button clicked  reset all grids to white
         
         gridCell.style.backgroundColor ='white'
        
@@ -38,16 +39,16 @@ for (i=0; i<totalsqure; i++){
     
 }
 
-function changeSquareSize() {
+function changeSquareSize() {  //change number of squares 
     let userInput;
-    let noSquares;
+    let noSquares;  //number of squares
    
     userInput = prompt('What is the number of squares that you want on axis?');
-    noSquares = parseInt(userInput);
+    noSquares = parseInt(userInput);  //input into int
     if (isNaN(noSquares)|| (noSquares>100) || (noSquares<=0)) {
         alert ('Invalid input. Please enter a valid number between from 1 to 100');
     }
-    else{
+    else{ //recreate grid  based on user input
         squarePerSide = noSquares
        const gridesize =600;
         sketchArea.innerHTML ='';
@@ -56,6 +57,6 @@ function changeSquareSize() {
     
 }
 
-noButton.addEventListener('click',changeSquareSize)
-createGrid()
+noButton.addEventListener('click',changeSquareSize) //number of square button
+createGrid() 
 
